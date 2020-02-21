e0cb21df61fd71aecfc1d049660977177f18883e
Noxheart <daniel-97@live.se>
2020-02-21 14:48:03 +0100

    linkatfiler

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 document.getElementById("skicka").onclick()
+
+function updateList()
+{
+  //Hämta alla personer i kö:
+  fetch('https://localhost:44352/api/queueitems')
+    .then(response => response.json())
+    .then(data => buildList(data))
+}
+
+function buildList(queueItems)
+{
+  //Ta bort alla föregående objekt så att listan uppdateras och inte fylls på hela tiden
+  document.getElementById("container").innerHTML = "";
+
+  queueItems.forEach(queueItem => {
+    //Skapa en div för varje rad
+    var newDiv = document.createElement("div");
+    newDiv.className = "addedDiv";
+    newDiv.textContent = queueItem.name;
+
+    //Skapa en div som håller all viktig data
+    var divData = document.createElement("div");
+    divData.type = "div";
+    divData.value = queueItem.id;
+    divData.name = queueItem.title; 
+
+    // lägg till elementen till DOM
+    var containerDiv = document.getElementById("container"); 
+    containerDiv.appendChild(newDiv);
+    newDiv.appendChild(divData);
+
+  });
+}
